15239cbf476dce79eb2aaa1967f2881cf13c610f
Dermot Logue <dermot.logue@dkit.ie>
2024-04-18 18:39:45 +0100

    variables renames for clarity

--- src/main/java/org/example/Server.java
+++ src/main/java/org/example/Server.java
@@ -37,41 +37,45 @@ public class Server {
     }
 
 
-
+    /**
+     * Read file length from socket, using a buffer, and write the received bytes to a local file.
+     *
+     * @param fileName - name for new local file to store received data
+     * @throws Exception
+     */
     private static void receiveFile(String fileName)
             throws Exception
     {
-        int bytes = 0;
-        FileOutputStream fileOutputStream = new FileOutputStream(fileName);
-
+     FileOutputStream fileOutputStream = new FileOutputStream(fileName);
 
         // DataInputStream allows us to read Java primitive types from stream e.g. readLong()
         // read the size of the file in bytes (the file length)
-        long size = dataInputStream.readLong();
-        System.out.println("Server: file size in bytes = " + size);
-
+        long bytes_remaining = dataInputStream.readLong(); // bytes remaining to be read (initially equal to file size)
+        System.out.println("Server: file size in bytes = " + bytes_remaining);
 
         // create a buffer to receive the incoming bytes from the socket
         byte[] buffer = new byte[4 * 1024];         // 4 kilobyte buffer
 
         System.out.println("Server:  Bytes remaining to be read from socket: ");
+        int bytes_read = 0;    // number of bytes read from the socket
 
         // next, read the raw bytes in chunks (buffer size) that make up the image file
-        while (size > 0 &&
-                (bytes = dataInputStream.read(buffer, 0,(int)Math.min(buffer.length, size))) != -1) {
+        while (bytes_remaining > 0 &&  (bytes_read =
+                dataInputStream.read(buffer, 0,(int)Math.min(buffer.length, bytes_remaining))) != -1) {
 
             // above, we read a number of bytes from stream to fill the buffer (if there are enough remaining)
             // - the number of bytes we must read is the smallest (min) of: the buffer length and the remaining size of the file
             //- (remember that the last chunk of data read will usually not fill the buffer)
 
             // Here we write the buffer data into the local file
-            fileOutputStream.write(buffer, 0, bytes);
+            fileOutputStream.write(buffer, 0, bytes_read);
 
-            // reduce the 'size' by the number of bytes read in.
-            // 'size' represents the number of bytes remaining to be read from the socket stream.
+            // reduce the 'bytes_remaining' to be read by the number of bytes read in.
+            // 'bytes_remaining' represents the number of bytes remaining to be read from the source file (via socket)
             // We repeat this until all the bytes are dealt with and the size is reduced to zero
-            size = size - bytes;
-            System.out.print(size + ", ");
+            bytes_remaining = bytes_remaining - bytes_read;
+
+            System.out.print(bytes_remaining + ", ");
         }
 
         System.out.println("File is Received");
