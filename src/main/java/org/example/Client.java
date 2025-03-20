dbbfa58461e960020b1a83dc8a0ca48aa943f586
logued <logued@dkit.ie>
2025-03-20 23:38:09 +0000

    updated comments

--- src/main/java/org/example/Client.java
+++ src/main/java/org/example/Client.java
@@ -1,17 +1,19 @@
-package org.example;
+package org.example;        // March 2025
 import java.io.*;
 import java.net.Socket;
 
 // Client Server - transfer binary data
 
-public class Client_Main {
+public class Client {
+    final static int SERVER_PORT = 1024;
+
     private static DataOutputStream dataOutputStream = null;
     private static DataInputStream dataInputStream = null;
 
     public static void main(String[] args)
     {
         // Create Client Socket connect to port 900
-        try (Socket socket = new Socket("localhost", 900)) {
+        try (Socket socket = new Socket("localhost", SERVER_PORT)) {
 
             dataInputStream = new DataInputStream(socket.getInputStream());
             dataOutputStream = new DataOutputStream( socket.getOutputStream());
@@ -35,15 +37,17 @@ public class Client_Main {
         File file = new File(path);
         FileInputStream fileInputStream = new FileInputStream(file);
 
-        // send the length (in bytes) of the file to the server
+        // send the length (in bytes) of the file to the server.
+        // The server expects this, as this is the Protocol.
         dataOutputStream.writeLong(file.length());
 
         // Here we break file into chunks
         byte[] buffer = new byte[4 * 1024]; // 4 kilobyte buffer
 
-        // read bytes from file into the buffer until buffer is full or we reached end of file
+        // read bytes from file into the buffer until buffer is full, or we reached end of file
         while ((bytes = fileInputStream.read(buffer))!= -1) {
-            // Send the buffer contents to Server Socket, along with the count of the number of bytes
+            // Send the buffer contents to the Server over our socket,
+            // along with the count of the number of bytes we are sending.
             dataOutputStream.write(buffer, 0, bytes);
             dataOutputStream.flush();   // force the data into the stream
         }
--- src/main/java/org/example/Server.java
+++ src/main/java/org/example/Server.java
@@ -1,30 +1,46 @@
-package org.example;
+package org.example;                // March 2025
+
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.FileOutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.sql.SQLOutput;
 
-// Server            April 2024
+// Server
+// This server awaits a connection from a client, and when a connection is made,
+// then a Socket is established between the client and server.
+// The server then waits for (image) data to arrive from the client, and when it does,
+// the data is read in batches and is written to a file.
+
 // InputStream could also be buffered for efficiency (BufferedInputStream)
 
 public class Server {
 
-    private static DataOutputStream dataOutputStream = null;
-    private static DataInputStream dataInputStream = null;
+    final static int SERVER_PORT = 1024;
 
-    public static void main(String[] args)
-    {
-        // Create Server Socket running on port 900
-        try (ServerSocket serverSocket = new ServerSocket(900)) {
-            System.out.println("Server is Starting in Port 900");
-            // Accept the Client request using accept method
+    private DataOutputStream dataOutputStream = null;
+    private DataInputStream dataInputStream = null;
+
+    public static void main(String[] args) {
+        Server server = new Server();
+        server.start();
+    }
+
+    public void start() {
+
+        // Create Server Socket on specific port number
+        try (ServerSocket serverSocket = new ServerSocket(SERVER_PORT)) {
+            System.out.println("Server is Starting in Port "+SERVER_PORT);
+
+            // Wait for a client to connect, and then set up a Socket to receive data
             Socket clientSocket = serverSocket.accept();
             System.out.println("Connected");
+
+            // Get input and output streams on the Socket to send/receive binary data
             dataInputStream = new DataInputStream(clientSocket.getInputStream());
-            dataOutputStream = new DataOutputStream( clientSocket.getOutputStream());
-            // Here we call receiveFile define new for that file
+            dataOutputStream = new DataOutputStream(clientSocket.getOutputStream());
+
+            // call function to extract file data from the data input stream and write to file
             receiveFile("images/parrot_image_received.jpg");
 
             dataInputStream.close();
@@ -38,28 +54,33 @@ public class Server {
 
 
     /**
-     * Read file length from socket, using a buffer, and write the received bytes to a local file.
+     * Read file length from the socket using a buffer, and write the received bytes to a local file.
      *
      * @param fileName - name for new local file to store received data
      * @throws Exception
      */
-    private static void receiveFile(String fileName)
+    private void receiveFile(String fileName)
             throws Exception
     {
      FileOutputStream fileOutputStream = new FileOutputStream(fileName);
 
-        // DataInputStream allows us to read Java primitive types from stream e.g. readLong()
-        // read the size of the file in bytes (the file length)
+        // DataInputStream allows us to read Java primitive types from stream
+        // e.g. readLong()
+        // The client sends us a count of the number of bytes that it will be sending
+        // to us.  This count is represented as a long int and occupies the first 8 bytes
+        // of the data sent through the socket.
+        // It is the length of the image file in bytes.
+
         long bytes_remaining = dataInputStream.readLong(); // bytes remaining to be read (initially equal to file size)
         System.out.println("Server: file size in bytes = " + bytes_remaining);
 
-        // create a buffer to receive the incoming bytes from the socket
+        // create a buffer to receive the incoming image bytes from the socket
         byte[] buffer = new byte[4 * 1024];         // 4 kilobyte buffer
 
         System.out.println("Server:  Bytes remaining to be read from socket: ");
         int bytes_read = 0;    // number of bytes read from the socket
 
-        // next, read the raw bytes in chunks (buffer size) that make up the image file
+        // next, read the raw bytes in chunks (of buffer size) that make up the image file
         while (bytes_remaining > 0 &&  (bytes_read =
                 dataInputStream.read(buffer, 0,(int)Math.min(buffer.length, bytes_remaining))) != -1) {
 
